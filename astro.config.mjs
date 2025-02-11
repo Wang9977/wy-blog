5dc73469c743b78a431a5ec76e793879cc397939
wangyuan198 <wangyuan198@jd.com>
2025-02-11 16:10:12 +0800

    test: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/dist',
+	base: '/wy-blogs',
 	site:'https://wang9977.github.io/wy-blogs',
 	integrations: [
 
