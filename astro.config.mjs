863b2db6f9db457a24e9ed83d31066476881f406
wangyuan198 <wangyuan198@jd.com>
2025-02-11 10:44:08 +0800

    feat: init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,29 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Blog',
+			logo: {src:'./src/assets/logo.webp'},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			// sidebar: [
+			// 	// {
+			// 	// 	// label:"首页",
+			// 	// 	slug: 'home/home'
+			// 	// },
+			// 	// {
+			// 	// 	label: 'Guides',
+			// 	// 	items: [
+			// 	// 		// Each item here is one entry in the navigation menu.
+			// 	// 		{ label: 'Example Guide1', slug: 'guides/example' },
+			// 	// 	],
+			// 	// },
+			// 	// {
+			// 	// 	label: 'Reference',
+			// 	// 	autogenerate: { directory: 'reference' },
+
+			// 	// },
+			// ],
 		}),
 	],
 });
